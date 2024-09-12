01cab12468ad7113f68d4155f94c27073730bf25
maheg <2468910@csjtean.qc.ca>
2024-09-12 09:01:34 -0400

    V3_1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ FOUNTAIN_RADIUS: Final = 1
 IDEAL_PATH_WIDTH: Final = 2
 
 # VARIABLES
-asphalt_in_truck: float  # Asking the user, in cubic meters.
+asphalt_in_truck: float  # Asking the user, in square meters.
 fountain_area: float  # Next 4 values will be calculated later
 total_area: float
 path_width: float
@@ -23,15 +23,15 @@ asphalt_needed: float
 fountain_area = pi * FOUNTAIN_RADIUS ** 2
 asphalt_needed = round(pi * (IDEAL_PATH_WIDTH + FOUNTAIN_RADIUS) ** 2 - fountain_area, 2)
 
-asphalt_in_truck = float(input("Enter the amount of asphalt left in the truck (in cubic meters): "))
+asphalt_in_truck = float(input("Enter the amount of asphalt left in the truck (in square meters): "))
 total_area = fountain_area + asphalt_in_truck
 
 path_width = round(sqrt(total_area / pi) - FOUNTAIN_RADIUS, 2)
 
 if asphalt_in_truck >= asphalt_needed:
     print(f"Path width : {path_width} METERS \n\n - For an ideal path of 2 meters - \nAsphalt needed for the path: "
-          f"{asphalt_needed} CUBIC METERS\nAsphalt left: {round(asphalt_in_truck - asphalt_needed, 2)} CUBIC METERS")
+          f"{asphalt_needed} SQUARE METERS\nAsphalt left: {round(asphalt_in_truck - asphalt_needed, 2)} SQUARE METERS")
     quit()
 
 print(f"Path width : {path_width} METERS \n\n - For an ideal path of 2 meters - \nAsphalt needed for the path: "
-      f"{asphalt_needed} CUBIC METERS\nAsphalt to add: {round(asphalt_needed - asphalt_in_truck, 2)} CUBIC METERS")
+      f"{asphalt_needed} SQUARE METERS\nAsphalt to add: {round(asphalt_needed - asphalt_in_truck, 2)} SQUARE METERS")
