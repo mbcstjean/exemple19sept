c15d4aab6ca798af2a3bcbdbcfa9b5428f8ef542
MB <2468910@cstjean.qc.ca>
2024-09-17 09:58:14 -0400

    V3_2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Author: GTX - MB 2024
 # Problem description : Calculate a path width around a fountain
-# Last modification on : September, 10, 2024
-# Known problems/bugs :
-
+# Last modification on : September, 12, 2024
+# Known problems/bugs : 1- If user enter a string in the asphalt_in_truck variable, program won't work
+#                       2-
 # IMPORTATIONS
 from math import pi, sqrt
 from typing import Final
@@ -20,18 +20,21 @@ asphalt_needed: float
 
 
 # LOGIC
-fountain_area = pi * FOUNTAIN_RADIUS ** 2
+fountain_area = pi * FOUNTAIN_RADIUS ** 2  # Fountain area in square meters
+# Asphalt needed to build a path of 2 meters of width
 asphalt_needed = round(pi * (IDEAL_PATH_WIDTH + FOUNTAIN_RADIUS) ** 2 - fountain_area, 2)
-
+# Ask the user to enter the amount of asphalt left in the truck
 asphalt_in_truck = float(input("Enter the amount of asphalt left in the truck (in square meters): "))
-total_area = fountain_area + asphalt_in_truck
+total_area = fountain_area + asphalt_in_truck  # Area of the fountain + the path around it
 
-path_width = round(sqrt(total_area / pi) - FOUNTAIN_RADIUS, 2)
+path_width = round(sqrt(total_area / pi) - FOUNTAIN_RADIUS, 2)  # Path width of the buildable path with the asphalt left
 
-if asphalt_in_truck >= asphalt_needed:
-    print(f"Path width : {path_width} METERS \n\n - For an ideal path of 2 meters - \nAsphalt needed for the path: "
-          f"{asphalt_needed} SQUARE METERS\nAsphalt left: {round(asphalt_in_truck - asphalt_needed, 2)} SQUARE METERS")
-    quit()
+if asphalt_in_truck >= asphalt_needed:  # Check if we have more asphalt than required
+    print(f"\nPath width with this amount of asphalt in the truck: {path_width} METERS \n\n "
+          f"- For an ideal path of 2 meters - \nAsphalt needed for the ideal path: {asphalt_needed} SQUARE METERS\n"
+          f"Asphalt left after path construction: {round(asphalt_in_truck - asphalt_needed, 2)} SQUARE METERS")
+    quit()  # Quit the program after printing the results
 
-print(f"Path width : {path_width} METERS \n\n - For an ideal path of 2 meters - \nAsphalt needed for the path: "
-      f"{asphalt_needed} SQUARE METERS\nAsphalt to add: {round(asphalt_needed - asphalt_in_truck, 2)} SQUARE METERS")
+print(f"\nPath width with this amount of asphalt in the truck: {path_width} METERS \n\n - For an ideal path of 2 meters"
+      f"- \nAsphalt needed for the ideal path: {asphalt_needed} SQUARE METERS\nAsphalt to add for path construction: "
+      f"{round(asphalt_needed - asphalt_in_truck, 2)} SQUARE METERS")
